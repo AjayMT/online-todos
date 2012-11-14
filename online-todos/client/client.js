cfc1b52e154bd28776ef968e12b06afb269bf353
AjayMT <ajay.tatachar@gmail.com>
2012-11-14 15:22:16 +0530

    Added basic lists and items, added Twitter Bootstrap styling.

--- online-todos/client/client.js
+++ online-todos/client/client.js
@@ -5,3 +5,27 @@ Template.mainContent.user = function () {
 Template.loginForm.destroyed = function () {
 	Meteor.absoluteUrl(Meteor.userId());
 }
+
+Template.todosUI.lists = function () {
+	return Lists.find({}, { sort: { name: 1 } });
+}
+
+Template.todosUI.activeList = function () {
+	if (Session.equals("currentList", this._id)) {
+		return "active";
+	}
+	return "";
+}
+
+Template.todosUI.events({
+	"click li.list": function () {
+		Session.set("currentList", this._id);
+	},
+	"click input.check": function () {
+		// Make item state completed.
+	}
+});
+
+Template.todosUI.items = function () {
+	return Items.find({ list: Session.get("currentList") }, { sort: { priority: -1 } });
+}
---
+++ online-todos/model.js
@@ -0,0 +1,2 @@
+Lists = new Meteor.Collection("lists");
+Items = new Meteor.Collection("items");
